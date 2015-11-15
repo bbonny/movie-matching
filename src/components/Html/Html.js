96a519c5a10abc36c1bc8d1d7c4fcc19b472dac8
Benjamin Bonny <benjamin.bonny@gmail.com>
2015-11-15 11:54:32 +0100

    use bootsrap css and react-bootstrap modeul

--- src/components/Html/Html.js
+++ src/components/Html/Html.js
@@ -38,6 +38,9 @@ class Html extends Component {
         <meta name="description" content={this.props.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
         <style id="css" dangerouslySetInnerHTML={{__html: this.props.css}} />
       </head>
       <body>
--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Input from 'react-bootstrap/lib/Input';
+import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
+
 import styles from './SearchBox.css';
 import withStyles from '../../decorators/withStyles';
 import Link from '../Link';
@@ -9,7 +13,19 @@ class SearchBox extends Component {
   render() {
     return (
       <div className="SearchBox">
-        <input type="text" autofocus placholder="Model" />
+        <Row className="show-grid">
+          <Col xs={3} md={4}></Col>
+          <Col xs={6} md={4}>
+            <Input
+              type="text"
+              placeholder="Enter text"
+              hasFeedback
+              ref="input"
+              groupClassName="group-class"
+              labelClassName="label-class" />
+          </Col>
+          <Col xs={3} md={4}></Col>
+        </Row>
       </div>
     );
   }
